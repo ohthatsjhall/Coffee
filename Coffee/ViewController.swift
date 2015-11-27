be2ac55659d405faf54f93c9d758f95c0d4f4a78
Justin Hall <jhall.auburn@gmail.com>
2015-11-27 06:40:34 -0800

    initial commit / market and tableview set up

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -7,8 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+  
+  @IBOutlet weak var mapView: MKMapView?
+  
+  var locationManager: CLLocationManager?
+  let distanceSpan: Double = 500
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    if let mapView = self.mapView {
+      mapView.delegate = self
+    }
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    if locationManager == nil {
+      locationManager = CLLocationManager()
+      
+      locationManager!.delegate = self
+      locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+      locationManager!.requestAlwaysAuthorization()
+      locationManager!.distanceFilter = 50
+      locationManager!.startUpdatingLocation()
+    }
+    
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,6 +46,15 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  
+  func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+    if let mapView = self.mapView {
+      let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+      mapView.setRegion(region, animated: true)
+    }
+  }
+  
 
 
 }
