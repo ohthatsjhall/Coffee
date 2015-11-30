065ac2b3f6f4bb197823caa2a92eb1c6df8c6a6b
Justin Hall <jhall.auburn@gmail.com>
2015-11-29 20:08:05 -0800

    added annotations for mapView

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 import MapKit
+import RealmSwift
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
   
   @IBOutlet weak var mapView: MKMapView?
   
+  var venues: Results<Venue>?
+  
+  var lastLocation: CLLocation?
   var locationManager: CLLocationManager?
   let distanceSpan: Double = 500
   
@@ -53,6 +57,50 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
       let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
       mapView.setRegion(region, animated: true)
     }
+  }
+  
+  func refreshVenues(location: CLLocation?, getDataFromFoursquare: Bool = false) {
+    if location != nil {
+      lastLocation = location
+    }
+    
+    if let location = lastLocation {
+      
+      if getDataFromFoursquare == true {
+        
+        CoffeeAPI.sharedInstance.getCoffeeShopsWithLocation(location)
+        
+      }
+      
+      let realm = try! Realm()
+      
+      venues = realm.objects(Venue)
+      
+      for venue in venues! {
+        
+        let annotation = CoffeeAnnotation(title: venue.name, subtitle: venue.address, coordinate: CLLocationCoordinate2D(latitude: Double(venue.latitude), longitude: Double(venue.longitude)))
+        
+        mapView?.addAnnotation(annotation)
+      }
+      
+    }
+  }
+  
+  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    
+    if annotation.isKindOfClass(MKUserLocation) {
+      return nil
+    }
+    
+    var view = mapView.dequeueReusableAnnotationViewWithIdentifier("annotationIdentifier")
+    
+    if view == nil {
+      view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotationIdentifier")
+    }
+    
+    view?.canShowCallout = true
+    
+    return view
   }
   
 
